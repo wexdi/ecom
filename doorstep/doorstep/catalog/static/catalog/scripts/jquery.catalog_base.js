32ec404609e4c04f1a926ba77982246556d66994
Deepak <info.support@wexdi.com>
2018-06-13 05:47:23 +0000

    remove doorstep

--- doorstep/doorstep/catalog/static/catalog/scripts/jquery.catalog_base.js
+++
@@ -1,4 +0,0 @@
-function basketChanged() {
-    // Initializing new forms in basket-container
-    bootstrapAjax($, '#basket-container');
-}
--- doorstep/doorstep/static/doorstep/scripts/base.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('[data-toggle="tooltip"]').tooltip();
-  $(document).on('click', '[data-href]:not(a)', function() {
-    window.location = $(this).attr('data-href');
-  });
-});
